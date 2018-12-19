3c4a3788fa1ac244ac9e8931cb4ba3e55c3870e0
Sean12mps <sean.michael.piettojo@gmail.com>
2018-12-20 00:50:27 +0700

    Add input text direction.

--- Tugas.java
+++ Tugas.java
@@ -6,17 +6,18 @@ public class Tugas{
 		Scanner scan = new Scanner(System.in);
 		int choose = 0;
 		do {
+			
 			System.out.println("1. Menu 1");
 			System.out.println("2. Menu 2");
 			System.out.println("3. Menu 3");
 			System.out.println("4. Menu 4");
 			System.out.println("5. Menu 5");
 			System.out.println("6. Exit");
-
-			System.out.print("Choose :");
+			System.out.print("Choose menu:");
 
 			choose = scan.nextInt();
 			scan.nextLine();
+			
 		}while(choose != 6);
 	}
 
