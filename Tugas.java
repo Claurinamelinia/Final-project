22c6d9ed3e1f3f891c7b9db14ad5d5cf7a2ea3d7
Claurinamelinia <claurina.triatma@gmail.com>
2018-12-20 23:42:11 +0700

    Add more functions

--- Tugas.java
+++ Tugas.java
@@ -3,8 +3,112 @@ public class Tugas{
 
 	public static void main(String [] args){
 		menu();
+	}
+
+	static void form(){
+		Scanner scan = new Scanner(System.in);
+		Vector<String>vname = new Vector<>();
+		Vector<String>vphone = new Vector<>();
+		Vector<String>vaddress = new Vector<>();
+		Vector<String>vorder = new Vector<>();
+		Vector<Integer>vquantity = new Vector<>();
+		Vector<Integer>vprices = new Vector<>();
+		Vector<Integer>vtotal = new Vector<>();
+
+		String name, address, order, phone ="";
+		int quantity = 0;
+		boolean phone_state = true;
+		int prices = 0;
+		int total = 0;
+
+		do {
+			System.out.print("Input name [3..30 character]:");
+			name = scan.nextLine();
+		}while(name.length() < 3 || name.length() > 30);
+
+		do {
+			System.out.print("Input phone [12 digit]:");
+			phone = scan.nextLine();
+			for(int i = 1; i < phone.length(); i++){
+				if(Character.isDigit(phone.charAt(i))){
+					phone_state = false;
+				}else{
+					phone_state = true;
+					break;
+				}
+			}
+		}while(phone.length() != 12 || phone_state);
+
+		do {
+			System.out.print("Input Address [must ends with 'Street']:");
+			address = scan.nextLine();
+		}while(!address.endsWith("Street"));
+
+		do {
+			System.out.print("Input order [Hoodie jumper | Hoodie Zipper | Basic sweater | Boomber jacket]:");
+			order = scan.nextLine();
+		}while(!order.equals("Hoodie jumper") && !order.equals("Hoodie zipper") && !order.equals("Basic sweater") && !order.equals("Boomber jacket"));
+
+		do {
+			try{
+				System.out.print("Input Quantity [1..25(must be numeric)]:");
+				quantity = scan.nextInt(); scan.nextLine();
+			}catch(Exception e){
+				quantity = 0;
+			}
+		}while(quantity < 1 || quantity > 25);
+
+		if(order.equals("Hoodie jumper"))total = quantity * 92000;
+		else if (order.equals("Hoodie zipper"))total = quantity * 100000;
+		else if(order.equals("Basic sweater"))total = quantity * 86000;
+		else if (order.equals("Boomber jacket"))total = quantity * 75000;
+
+		System.out.println("Total Price"+total);
+
+
+		vname.add(name);
+		vphone.add(phone);
+		vaddress.add(address);
+		vorder.add(order);
+		vquantity.add(quantity);
+		vprices.add(prices);
+		vtotal.add(total);
 
 	}
+
+
+/*
+    static void listProduct(){
+		Scanner scan = new Scanner(System.in);
+		//aray//
+		String [] products = new String [5];
+	    products [0] = "Hoodie jumper";
+		products [1] = "Hoodie zipper";
+		products [2] = "Basic sweater";
+		products [3] = "Boomber jacket";
+		 System.out.println(products [0]);
+
+		int [] prices = new int [5];
+		prices [0] = 92000;
+		prices [1] = 100000;
+		prices [2] = 86000;
+		prices [3] = 75000;
+			System.out.println(prices [0]);
+
+		int [] quantity = new int [5];
+		quantity [0] = 92000;
+		quantity [1] = 100000;
+		quantity [2] = 86000;
+		quantity [3] = 75000;
+			System.out.println(quantity [0]);
+
+		for (int i = 0; i<4; i++){
+			System.out.println(products[i]);
+			System.out.println(prices[i]);
+			System.out.println(quantity[i]);
+
+
+	} */
 
 	static void menu(){
 		Scanner scan = new Scanner(System.in);
@@ -12,11 +116,15 @@ public class Tugas{
 		String name="";
 		do {
 
-			System.out.println("1. Name");
-			System.out.println("2. Menu 2");
-			System.out.println("3. Menu 3");
-			System.out.println("4. Menu 4");
-			System.out.println("5. Menu 5");
+			System.out.println("               ========================*========================");
+			System.out.println("               |             THIS IS LABELLECLOTHING           | ");
+			System.out.println("               ========================*========================");
+
+			System.out.println("1. List of Product");
+			System.out.println("2. Order form");
+			System.out.println("3. Canceling ");
+			System.out.println("4. Checkout");
+			System.out.println("5. Payment");
 			System.out.println("6. Exit");
 			System.out.print("Choose menu:");
 
@@ -25,9 +133,16 @@ public class Tugas{
 
 			switch(choose){
 				case 1:
-					do {
-						System.out.println("Budi");
-					}while(name.equals("Budi"));
+				//listProduct();
+				break;
+				case 2:
+					form();
+				break;
+				case 3:
+				break;
+				case 4:
+				break;
+				case 5:
 				break;
 			}
 
